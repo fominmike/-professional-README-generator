7d94f314225110997070172a5593abf32840dfe2
Mike Fomin <mikefomin@Mikes-MacBook-Pro.local>
2022-12-14 12:05:08 -0800

    added stuuf

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,55 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer')
+const fs = require('fs')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        name: 'title',
+        type: 'input',
+        message: 'What is the title of your project?'
+    },
+    {
+        name: 'description',
+        type: 'input',
+        message: 'Enter in a description'
+    },
+    {
+        name:'installation',
+        type: 'input',
+        message: 'wrfg',
+    },
+    {
+        name:'wefw',
+        type: 'input',
+        message: 'wef',
+    },
+    {
+        name:'',
+        type: 'input',
+        message:'',
+    },
+    {
+        name:'',
+        type: 'input',
+        message:'',
+    },
+    {
+        name:'',
+        type: 'input',
+        message:'',
+    },
+    {
+        name:'',
+        type: 'input',
+        message:'',
+    },
+    {
+        name:'',
+        type: 'input',
+        message:'',
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
